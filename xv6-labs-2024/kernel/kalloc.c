f70bea56735254321d330b54c16dd7ba8b41b09e
Tu Vi <caothituvi201@gmail.com>
2025-11-26 19:29:45 +0700

    first commit

--- xv6-labs-2024/kernel/kalloc.c
+++ xv6-labs-2024/kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+// implement of getFreemem
+uint64 getFreemem(void) {
+  //lock memory
+  acquire(&kmem.lock);
+  struct run *r = kmem.freelist;
+  uint64 count = 0;
+  while(r){
+    count++;
+    r = r->next;
+  }
+  //release memory lock
+  release(&kmem.lock);
+  return count * PGSIZE;
+}
